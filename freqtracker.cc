099dc2d8d26c6519484af3a3788c1146a808fc78
molpopgen <krthornt@uci.edu>
2020-12-23 12:54:23 -0800

    Use nested unordered_maps to track trajectories, then lift over to dict when the user wants the data.

--- freqtracker.cc
+++ freqtracker.cc
@@ -1,5 +1,6 @@
 #include <tuple>
 #include <cstdint>
+#include <unordered_map>
 #include <map>
 #include <vector>
 
@@ -18,12 +19,12 @@ struct FreqTracker
 
     using trajectory = std::vector<std::uint32_t>;
 
-    // Hashed associative container: key -> frequency trajectory
-    // We are being a bit inefficient here, using a red-black
-    // tree instead of a hashed container.  The reason is that C++
-    // does not provide std::hash for tuples, even tuples of POD.
-    // We could fix that, but won't here.
-    using map_type = std::map<key_type, trajectory>;
+    // We record trajectories by nesting hashed associative containers.
+    // The outer hash is on origin time.  The inner is on position.
+    // Finally, the pair is effect size + the frequency trajectory.
+    using map_type
+        = std::unordered_map<std::uint32_t,
+                             std::unordered_map<double, std::pair<double, trajectory>>>;
 
     map_type trajectories;
     std::uint32_t burin_time;
@@ -42,22 +43,63 @@ struct FreqTracker
                     {
                         if (pop.mcounts[i] > 0 && pop.mcounts[i] < 2 * pop.N)
                             {
-                                auto key = std::make_tuple(pop.mutations[i].g,
-                                                           pop.mutations[i].pos,
-                                                           pop.mutations[i].s);
-                                auto itr = trajectories.find(key);
-                                if (itr == end(trajectories))
+                                auto origin = pop.mutations[i].g;
+                                auto origin_itr = trajectories.find(origin);
+
+                                if (origin_itr == end(trajectories))
                                     {
-                                        trajectories.emplace(std::move(key),
+                                        trajectories[origin][pop.mutations[i].pos]
+                                            = std::make_pair(pop.mutations[i].s,
                                                              trajectory{pop.mcounts[i]});
                                     }
                                 else
                                     {
-                                        itr->second.push_back(pop.mcounts[i]);
+                                        auto pos_itr = origin_itr->second.find(
+                                            pop.mutations[i].pos);
+                                        if (pos_itr == end(origin_itr->second))
+                                            {
+                                                origin_itr->second[pop.mutations[i].pos]
+                                                    = std::make_pair(
+                                                        pop.mutations[i].s,
+                                                        trajectory{pop.mcounts[i]});
+                                            }
+                                        else
+                                            {
+                                                pos_itr->second.second.push_back(
+                                                    pop.mcounts[i]);
+                                            }
                                     }
                             }
                     }
             }
+    }
+
+    // Make a copy of the data that Python will see
+    // as a dict.
+    // While we could rely on pybind11 to auto-convert
+    // from a C++ map to a Python dict, doing so
+    // would give 3 total copies in memory due to
+    // temporaries.  So instead, we go straight
+    // to a dict using pybind11's API.
+    py::dict
+    get_trajectories() const
+    {
+        py::dict rv;
+
+        for (auto &&origin : trajectories)
+            {
+                for (auto &&position : origin.second)
+                    {
+                        // Make a Python tuple
+                        auto key = py::make_tuple(origin.first, position.first,
+                                                  position.second.first);
+                        // Rely on auto-conversion of c++ vector
+                        // to Python list, via pybind11/stl.h included above.
+                        rv[key] = position.second.second;
+                    }
+            }
+
+        return rv;
     }
 };
 
@@ -67,7 +109,7 @@ PYBIND11_MODULE(freqtracker, m)
         .def(py::init<std::uint32_t>(), py::arg("burnin_time")) // This is __init__
         .def("__call__",
              &FreqTracker::operator()) // This makes it a valid Python callable
-        .def_readonly(
+        .def_property_readonly(
             "trajectories",
-            &FreqTracker::trajectories); // Get the data as a dict in Python.
+            &FreqTracker::get_trajectories); // Get the data as a dict in Python.
 }
